5bd48f7513e307a9310e94348134a171d812a30c
dave <dave@vadedesign.com>
2022-01-02 14:54:00 +0800

    Created a Base layout template and added partials

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
@@ -18,6 +18,9 @@ async function main() {
     console.log('MONGO CONNECTION OPEN');
 }
 
+const app = express();
+
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
