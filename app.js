54f449b74e8c65d4dcfc952eaf538b9e54c1c3c9
dave <dave@vadedesign.com>
2022-01-10 19:42:28 +0800

    Completed Error Handling Setup

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
+const { campgroundSchema } = require('./schemas.js');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
+//Mongoose connection
 main().catch((err) => {
    console.log('CONNECTION ERROR:');
    console.log(err);
@@ -29,6 +31,17 @@ app.set('views', path.join(__dirname, 'views'));
 //Parse incoming form submissions so it won't return empty
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+//validation
+const validateCampground = (req, res, next) => {
+   const { error } = campgroundSchema.validate(req.body);
+   if (error) {
+      const msg = error.details.map((el) => el.message).join(',');
+      throw new ExpressError(msg, 400);
+   } else {
+      next();
+   }
+};
 
 app.get('/', (req, res) => {
    res.render('home');
@@ -53,8 +66,9 @@ app.get('/campgrounds/new', (req, res) => {
 //set Route as a post where the new form is submitted to
 app.post(
    '/campgrounds',
+   validateCampground,
    catchAsync(async (req, res, next) => {
-      if (!req.body.campground) throw new ExpressError('Invalid Campground Data');
+      // if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
       const campground = new Campground(req.body.campground);
       await campground.save();
       res.redirect(`/campgrounds/${campground._id}`);
@@ -82,6 +96,7 @@ app.get(
 //Add a Put/Patch Route for edit campground form
 app.put(
    '/campgrounds/:id',
+   validateCampground,
    catchAsync(async (req, res) => {
       const { id } = req.params;
       const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
